dd377c05b1a849b3db5c260e5eb230b8c2bc5100
Keerthana K <keerthanaaa2816@gmail.com>
2026-09-01 21:08:38 +0530

    Maximum Number of Vowels

---
+++ DSA/Sliding Window/Maximum Number of Vowels.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int left = 0;
+        int count = 0;
+        int maxcount = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+            
+            if(isVowel(s.charAt(right))) {
+                count++;
+            }
+
+            if(right - left + 1 == k) {
+                maxcount = Math.max(maxcount , count);
+
+                if(isVowel(s.charAt(left))) {
+                    count--;
+                }
+                left++;
+            }
+        } 
+        return maxcount;
+    }
+    private boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
